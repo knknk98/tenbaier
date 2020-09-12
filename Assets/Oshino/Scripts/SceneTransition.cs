5b71258a04608c851f2aeb736c6f0c917943ed51
kayo <kayooshino@gmail.com>
2020-09-12 16:22:27 +0900

    リザルト画面のスコア・ハイスコア表示、ハイスコア更新処理を実装

---
+++ Assets/Oshino/Scripts/SceneTransition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string DestinationScene;
+    public void ScreeTransition(){
+        SceneManager.LoadScene(DestinationScene);
+    }
+}
+
