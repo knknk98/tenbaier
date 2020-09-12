8e5f75667f70f3a41c233b26074af1f6df3dfa54
aguroshou <aguroshou0413@gmail.com>
2020-09-12 23:59:20 +0900

    動く暗い空の背景を追加しました。

--- Assets/Aguro/Scripts/MoveStagePattern.cs
+++ Assets/Aguro/Scripts/MoveStagePattern.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class MoveStagePattern : MonoBehaviour
 {
-    //マイナスの値で左に移動
-    //staticにしているので、これを変更すればステージ全体の移動速度が変わります
-    //GenerateStagePattern.csでステージ全体の移動速度を調整しています
-    public static float stagePatternMoveSpeed;
-    [SerializeField] float destroyStagePointX;
+
+    float destroyStagePointX;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +16,7 @@ public class MoveStagePattern : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(stagePatternMoveSpeed, 0, 0);
+        transform.Translate(GenerateStageAndBackground.stagePatternMoveSpeed, 0, 0);
         if (transform.position.x < destroyStagePointX)
         {
             Destroy(this.gameObject);
