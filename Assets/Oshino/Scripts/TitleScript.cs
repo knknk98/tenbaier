523bfc5531bf7ac6b9e08d5c2af790a5b0b97aa1
kayo <kayooshino@gmail.com>
2020-09-12 14:50:48 +0900

    タイトル画面のゲームスタートボタンを実装

---
+++ Assets/Oshino/Scripts/TitleScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScript : MonoBehaviour
+{
+    public void GameStart(){
+        // TODO: 遷移先を本番用シーンに変える
+        SceneManager.LoadScene("SampleScene");
+    }
+}
