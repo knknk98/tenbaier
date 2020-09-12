3d9b67670b6e2a1da018e7265d3ef81cf0766800
aguroshou <aguroshou0413@gmail.com>
2020-09-12 22:14:05 +0900

    GenerateStagePattern.csにGameOver関数とステージの動きを止める処理を追加しました

--- Assets/Aguro/Scripts/GenerateStagePattern.cs
+++ Assets/Aguro/Scripts/GenerateStagePattern.cs
@@ -7,24 +7,48 @@ public class GenerateStagePattern : MonoBehaviour
     [SerializeField] List<GameObject> stagePatternObjectList = new List<GameObject>();
     [SerializeField] float generateStagePointX;
 
-    //一番右にあるステージオブジェクトを保存するために必要
+    //一番右にあるステージオブジェクトを保存するため
     //Inspector上で右端のステージオブジェクトを代入する必要があります。
-    [SerializeField] private GameObject rightSideStagePatternObject;
+    [SerializeField] GameObject rightSideStagePatternObject;
+
+    //ステージが時間につれて速くなっていく加速度
+    [SerializeField] float stageSpeedAcceleration;
+
+    //スピードはマイナスの値なので、最大マイナス値を制限
+    [SerializeField] float stageSpeedMax;
+
+    [SerializeField] float initialStageSpeed;
+
+    [SerializeField] bool isGameOver;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGameOver = false;
+        MoveStagePattern.stagePatternMoveSpeed = initialStageSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isGameOver==true)
+        {
+            return;
+        }
+        if (MoveStagePattern.stagePatternMoveSpeed>=stageSpeedMax)
+        {
+        MoveStagePattern.stagePatternMoveSpeed += stageSpeedAcceleration;
+        }
 
         if (rightSideStagePatternObject.transform.position.x<generateStagePointX)
         {
-            //Instantiate( 生成するオブジェクト,  場所, 回転 );  回転はそのままなら↓
             rightSideStagePatternObject=Instantiate(stagePatternObjectList[Random.Range(0, 2)], this.transform.position, Quaternion.identity);
         }
+    }
+
+    public void GameOver()
+    {
+        MoveStagePattern.stagePatternMoveSpeed = 0.0f;
+        isGameOver = true;
     }
 }
--- Assets/Aguro/Scripts/MoveStagePattern.cs
+++ Assets/Aguro/Scripts/MoveStagePattern.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class MoveStagePattern : MonoBehaviour
 {
     //マイナスの値で左に移動
-    //staticにしているので、これを変更すればステージ全体の移動速度が変わると思います
-    static float stagePatternMoveSpeed;
+    //staticにしているので、これを変更すればステージ全体の移動速度が変わります
+    //GenerateStagePattern.csでステージ全体の移動速度を調整しています
+    public static float stagePatternMoveSpeed;
     [SerializeField] float destroyStagePointX;
 
     // Start is called before the first frame update
     void Start()
     {
-        stagePatternMoveSpeed = -0.05f;
         destroyStagePointX = -36f;
     }
 
