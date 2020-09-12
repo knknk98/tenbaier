ab2ede22eaf1bdb702a24bf7f98349c1f0d906d2
kayo <kayooshino@gmail.com>
2020-09-12 15:29:40 +0900

    タイトル画面のハイスコア表示機能を実装

---
+++ Assets/Oshino/Scripts/HighScoreText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreText : MonoBehaviour
+{
+    public Text highScoreText;          //ハイスコアを表示するText
+    private int highScore;              //ハイスコア用変数
+    private string key = "HighScore";   //ハイスコアの保存先キー
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ハイスコア情報取得。情報がなければ0にする
+        highScore = PlayerPrefs.GetInt(key,0);
+        // ハイスコア表示
+        highScoreText.text = "HighScore:" + highScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
