4a83a3c7270714152500f2f3b294582f73fd753d
nozomi203 <63065128+nozomi203@users.noreply.github.com>
2020-09-12 20:34:56 +0900

    ファイル構造変更，アイテム管理用のスクリプタブルオブジェクト追加

---
+++ Assets/Nozomi/Scripts/ItemScriptalbleObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class ItemScriptalbleObject : ScriptableObject
+{
+    public const string PATH = "ItemScriptableObject";
+    //MyScriptableObjectの実体
+    private static ItemScriptalbleObject singletonInstance;
+    public  static ItemScriptalbleObject  SingletonInstance{
+        get{
+            //初アクセス時にロードする
+            if(singletonInstance == null){
+                singletonInstance = Resources.Load<ItemScriptalbleObject>(PATH);
+
+                //ロード出来なかった場合はエラーログを表示
+                if(singletonInstance == null){
+                    Debug.LogError(PATH + " not found");
+                }
+            }
+
+            return singletonInstance;
+        }
+    }
+    
+    public List<ItemData> ItemDataList = new List<ItemData>();
+}
+
+[System.Serializable]
+public struct ItemData
+{
+    public Sprite thumbnail;
+    public string name;
+    public int price;
+}
