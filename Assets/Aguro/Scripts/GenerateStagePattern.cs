6e6884baf793880049f568c4eba1d753fa3fd7e6
aguroshou <aguroshou0413@gmail.com>
2020-09-12 17:33:43 +0900

    平面のステージが左に進むだけの状態です。ステージの自動生成はできていません。

---
+++ Assets/Aguro/Scripts/GenerateStagePattern.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateStagePattern : MonoBehaviour
+{
+    [SerializeField] List<GameObject> stagePatternObjectList = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Instantiate( 生成するオブジェクト,  場所, 回転 );  回転はそのままなら↓
+            //Instantiate(target, new Vector3(1.0f, 2.0f, 0.0f), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Aguro/Scripts/MoveStagePattern.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStagePattern : MonoBehaviour
+{
+    //マイナスの値で左に移動
+    [SerializeField] float stagePatternMoveSpeed;
+    [SerializeField] float stageDestroyPointX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(stagePatternMoveSpeed, 0, 0);
+        if (transform.position.x < stageDestroyPointX)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
